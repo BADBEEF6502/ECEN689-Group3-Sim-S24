685ab1c7975a504d7f44fd3549cf7bcbdaa85eae
HarrisGoats <112510052+HarrisGoats@users.noreply.github.com>
2024-04-18 11:10:58 -0500

    Update network.py

--- network.py
+++ network.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+import time
 
+#sigmod
 def sigmoid(matrix):
     return 1 / (1 + np.exp(-matrix))
 
@@ -10,17 +12,18 @@ def sigmoid_prime(matrix):
 
 
 # Free phase is calculated with beta set to zero, AKA ignore beta and mean square error
-def free_phase(X, batch_size, hidden_weights, hidden_size, out_weights, output_size, learning_rate):
-    hidden_old = np.random.randn(batch_size, hidden_size)
-    output_old = np.random.randn(batch_size, output_size)
-    for i in range(100):
+def free_phase(X, batch_size, hidden_weights, out_weights, learning_rate):
+    hidden_old = np.random.randn(batch_size, hidden_weights.shape[1])
+    output_old = np.random.randn(batch_size, out_weights.shape[1])
+    for i in range(40):
         # Continually calculate dE/dW_ij and hidden layer until convergence (*.T means transpose)
-        dEdW_ij = sigmoid_prime(hidden_old) * ((X@hidden_weights) + (output_old@out_weights.T)) - hidden_old
-        dEdW_ij2 = sigmoid_prime(output_old) * (hidden_old@out_weights) - output_old
+        dHidden = sigmoid_prime(hidden_old) * ((X@hidden_weights) + (output_old@out_weights.T)) - hidden_old
+        dOut = sigmoid_prime(output_old) * (hidden_old@out_weights ) - output_old
         
+        #print(np.sum(dHidden))
         # Update the layers
-        hidden_old = sigmoid(hidden_old + learning_rate*dEdW_ij)
-        output_old = sigmoid(output_old + learning_rate*dEdW_ij2)
+        hidden_old = sigmoid(hidden_old + learning_rate*dHidden)
+        output_old = sigmoid(output_old + learning_rate*dOut)
     
     # Return the calculated hidden and output layers
     return (hidden_old, output_old)
@@ -28,30 +31,36 @@ def free_phase(X, batch_size, hidden_weights, hidden_size, out_weights, output_s
 
 
 
-def clamped_phase(X, batch_size, hidden_weights, hidden_size, out_weights, output_size, learning_rate, y):
-    hidden_old = np.random.randn(batch_size, hidden_size)
-    output_old = np.random.randn(batch_size, output_size)
+def clamped_phase(X, hidden_old, output_old, hidden_weights, out_weights, learning_rate, y):
+
     for i in range(20):
-        hidden_free = sigmoid_prime(hidden_old) * ( X@hidden_weights + output_layer@out_weights.T) - hidden_old
-        output_free = sigmoid_prime(output_old) * (hidden_layer@out_weights) - output_old
-        dCdW_ij = output_free + beta * (y - output_old)
+        dHidden = sigmoid_prime(hidden_old) * ( X@hidden_weights + output_old@out_weights.T) - hidden_old
+        dOut = sigmoid_prime(output_old) * (hidden_old@out_weights) - output_old
+        dOut = dOut + beta * (y - output_old)
 
         # Update layers
-        hidden_old = hidden_free
-        output_old = sigmoid(output_old + learning_rate*dCdW_ij)
+        hidden_old = sigmoid(hidden_old + learning_rate*dHidden)
+        output_old = sigmoid(output_old + learning_rate*dOut)
 
 
     return (hidden_old, output_old)
 
-def train_net(epocs, batch_size, data, label, hidden_weights,hidden_size, out_weights, output_size, alpha_1, alpha_2, beta, learning_rate):
+def train_net(epocs, batch_size, data, label, hidden_weights, out_weights, alpha_1, alpha_2, beta, learning_rate, X_test, y_test):
     #break if batch size isnt  even (simplifys code)
     if(np.shape(data)[0]%batch_size!=0):
         raise Exception("data must be divisible by batch size!")
+    #finds number of iterations per batch
+    iterations = np.shape(data)[0]//batch_size
+    
+    mse_list=np.zeros((epocs*iterations))
+    accuracy_list=np.zeros((epocs*iterations))
+    test_acc= np.zeros((epocs))
+    test_acc_x = (np.arange(epocs)+1)*iterations
+    mse_index=0
     
     #starts epoc
     for epoc in range(epocs):
-        #starts batch
-        for batch in range(np.shape(data)[0]//batch_size):
+        for batch in range(iterations):
             #grabs X and Y data
             X = data[batch*batch_size:(batch+1)*batch_size]
             y0 = label[batch*batch_size:(batch+1)*batch_size]
@@ -61,38 +70,68 @@ def train_net(epocs, batch_size, data, label, hidden_weights,hidden_size, out_we
 
             
             #gets U of free and clamped phase
-            h_free, out_free = free_phase(X, batch_size, hidden_weights,hidden_size, out_weights,output_size, learning_rate)
-            h_clamped, out_clamped = clamped_phase(X, batch_size, hidden_weights,hidden_size, out_weights,output_size, learning_rate, y)
-            
-            #calcualtes dW
-            dW = alpha_1 * (1 / beta)* (1/1) *(1/batch_size)* ((X).T@(h_clamped) - (X).T@(h_free))
-            dW2 = alpha_2 * (1 / beta) * (1/1) *(1/batch_size)* ((h_clamped).T@(out_clamped) - (h_clamped).T@(out_free))
-            
-            #updates W
-            hidden_weights += dW
-            out_weights += dW2
+            h_free, out_free = free_phase(X, batch_size, hidden_weights, out_weights, learning_rate)
+            h_clamped, out_clamped = clamped_phase(X, h_free,out_free, hidden_weights, out_weights, learning_rate, y)
             
             # Determine if the guess is correct using the free phase
             # USE FORWARD PROP
+            mse = (1.0 / batch_size) * np.sum((y - out_free) ** 2) 
+            accuracy = np.sum((np.argmax(out_free,1)==np.argmax(y,1)))/batch_size
+            mse_list[mse_index]=mse
+            accuracy_list[mse_index]=accuracy
+            mse_index+=1
             if(batch%50==0):
-                h = sigmoid(X @ hidden_weights)
-                out = sigmoid(h @ out_weights)
-                print(np.argmax(out,1)-np.argmax(y,1))
-                mse = np.sum((np.argmax(out,1)==np.argmax(y,1)))
-                print('batch num:', batch ,' accuracy:', mse/batch_size)
+                #print(np.argmax(out_free,1),np.argmax(y,1))
+                ##print(out)
+                print('epoc', epoc,'batch num:', batch ,' accuracy:', accuracy, 'MSE',mse)
+                
+            
+            dW4 = np.zeros((64,hidden_weights.shape[1]))
+            dW5 = np.zeros((hidden_weights.shape[1],10))
+            
+            for i in range(batch_size):
+                dW4+=(np.outer(X[i],h_clamped[i])-np.outer(X[i],h_free[i]))
+                #dW6+=np.outer(X[i],h_clamped[i])-np.outer(X[i],h_free[i])
+                dW5+=(np.outer(h_clamped[i],out_clamped[i])-np.outer(h_free[i],out_free[i]))
+                
+            #calcualtes dW
+            dW4 = alpha_1 * (1 / beta) *(dW4 )
+            dW5 = alpha_2 * (1 / beta) *(dW5)
+            
+            #updates W
+            hidden_weights += dW4
+            out_weights += dW5
+            
+        if(epoc==1 or epoc==3):
+            alpha_1/=10
+            alpha_2/=10
+
+        test_acc[epoc]= test_net(X_test, y_test,hidden_weights ,out_weights)
+            
+            
             
             
 
-            
+    return (mse_list, accuracy_list, test_acc, test_acc_x) 
     print('done')
+    
+
+def test_net(X_test, y_test, W1, W2):
+    y = np.zeros((y_test.size, 10))
+    y[np.arange(y_test.size), y_test] = 1
+    batch_size = X_test.shape[0]
+    h_free, out_free = free_phase(X_test, batch_size, W1, W2, 1)
+    
+    accuracy = np.sum((np.argmax(out_free,1)==np.argmax(y,1)))/batch_size
+    return accuracy
 
 # Initialize network size
 input_size = 8 * 8
-hidden_size = 32
+hidden_size = 64
 output_size = 10
 
 # Add learning rate
-learning_rate = 0.1
+learning_rate = 1
 
 # Initialize input and output layers
 input_layer = np.zeros((1, input_size))
@@ -110,14 +149,36 @@ data = mnist_data['y']
 
 
 # Initialize variables related to model generation
-epoch = 10
+epoch = 6
 batch_count = 100
 batch_size = 100
 
 # Equilibrium propagation parameters
-alpha_1 = 0.01
-alpha_2 = 0.005
+alpha_1 = 0.1
+alpha_2 = 0.05
 beta = 1
 data, x_test, label, y_test = train_test_split(data, label, test_size=0.1)
 
-train_net(epoch, batch_size, data, label, hidden_weights, hidden_size, out_weights, output_size, alpha_1, alpha_2, beta, learning_rate )
+start = time.time()
+mse_list,accuracy_list, test_acc, test_acc_x = train_net(epoch, batch_size, data, label, hidden_weights, out_weights, alpha_1, alpha_2, beta, learning_rate, x_test, y_test )
+end = time.time()
+print(end-start)
+
+fig, ax = plt.subplots()
+plt.title("MSE and accuracy of Equalibrium Prop")
+mse_x=np.arange(len(mse_list))
+
+MSE = ax.plot(mse_list, color='tab:blue', label='MSE')
+
+ax.tick_params(axis='y', labelcolor='tab:blue' )
+#ax.legend()
+ax2 = ax.twinx()
+acc=ax2.scatter(test_acc_x, test_acc, color='tab:green', label = 'Test Accuracy')
+ax2.plot(accuracy_list, color='tab:orange', label = 'Batch Accuracy')
+
+ax2.set_ylim(0,1)
+acc.set_zorder(20)
+ax2.tick_params(axis='y', labelcolor='tab:orange')
+#fig.legend(loc='upper right')
+plt.grid()
+plt.show()
