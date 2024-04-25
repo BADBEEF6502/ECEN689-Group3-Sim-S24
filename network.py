a3509191e1d10617acd222ef5017efbd22adaca1
EyobelB <eyobel.berhane@gmail.com>
2024-04-24 19:33:31 -0500

    Reapply "Add gitignore, create graphs"
    This reverts commit 8e057875018ad3ed0326329cebe323e56d1485f4.

--- network.py
+++ network.py
@@ -3,6 +3,12 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import time
 
+
+# Setup an array to store free phase and total energy values
+total_energy = []
+free_energy = []
+clamped_energy = []
+
 #sigmod
 def sigmoid(matrix):
     return 1 / (1 + np.exp(-matrix))
@@ -25,6 +31,19 @@ def free_phase(X, batch_size, hidden_weights, out_weights, learning_rate):
         hidden_old = sigmoid(hidden_old + learning_rate*dHidden)
         output_old = sigmoid(output_old + learning_rate*dOut)
     
+    # Calculate new values at hidden layer and output layer
+    hidden_layer_values = sigmoid(X @ hidden_weights)
+    output_layer_values = sigmoid(hidden_layer_values @ out_weights)
+    # Load in the latest free phase energy value
+    energy_val = 0.0
+    for i in range(hidden_size):
+        energy_val += (hidden_layer_values[0][i] * hidden_layer_values[0][i])
+        for j in range(output_size):  
+            if i != j:     
+                energy_val -= out_weights[i][j] * hidden_layer_values[0][i] * output_layer_values[0][j]
+    energy_val /= 2
+    free_energy.append(energy_val)
+
     # Return the calculated hidden and output layers
     return (hidden_old, output_old)
 
@@ -41,7 +60,12 @@ def clamped_phase(X, hidden_old, output_old, hidden_weights, out_weights, learni
         # Update layers
         hidden_old = sigmoid(hidden_old + learning_rate*dHidden)
         output_old = sigmoid(output_old + learning_rate*dOut)
-
+    
+    # Create mean squared error here
+    # Calculate new values at hidden layer and output layer
+    hidden_layer_values = sigmoid(X @ hidden_weights)
+    output_layer_values = sigmoid(hidden_layer_values @ out_weights)
+    clamped_energy.append(0.5 * np.abs(y - output_layer_values).sum() * np.abs(y - output_layer_values).sum())
 
     return (hidden_old, output_old)
 
@@ -162,23 +186,75 @@ data, x_test, label, y_test = train_test_split(data, label, test_size=0.1)
 start = time.time()
 mse_list,accuracy_list, test_acc, test_acc_x = train_net(epoch, batch_size, data, label, hidden_weights, out_weights, alpha_1, alpha_2, beta, learning_rate, x_test, y_test )
 end = time.time()
-print(end-start)
+print(end-start, "second runtime")
 
-fig, ax = plt.subplots()
-plt.title("MSE and accuracy of Equalibrium Prop")
-mse_x=np.arange(len(mse_list))
+# Generate the free energy plot
+plt.plot(range(len(free_energy)), free_energy)
+plt.title("Free Energy over time")
+plt.xlabel("Episode #")
+plt.ylabel("Free Energy")
+plt.savefig("free_energy.png")
+plt.close()
 
-MSE = ax.plot(mse_list, color='tab:blue', label='MSE')
+# Generate the free energy plot with averaging filter applied
+avg_filter = np.ones(25) / 25
+plt.plot(range(len(free_energy)), np.convolve(free_energy, avg_filter, mode='same'))
+plt.title("Free Energy over time")
+plt.xlabel("Episode #")
+plt.ylabel("Free Energy")
+plt.savefig("free_energy_smoothed.png")
+plt.close()
 
-ax.tick_params(axis='y', labelcolor='tab:blue' )
-#ax.legend()
-ax2 = ax.twinx()
-acc=ax2.scatter(test_acc_x, test_acc, color='tab:green', label = 'Test Accuracy')
-ax2.plot(accuracy_list, color='tab:orange', label = 'Batch Accuracy')
+# Generate the clamped energy plot
+plt.plot(range(len(clamped_energy)), clamped_energy)
+plt.title("Clamped Energy over time")
+plt.xlabel("Episode #")
+plt.ylabel("Clamped Energy")
+plt.savefig("clamped_energy.png")
+plt.close()
 
-ax2.set_ylim(0,1)
-acc.set_zorder(20)
-ax2.tick_params(axis='y', labelcolor='tab:orange')
-#fig.legend(loc='upper right')
-plt.grid()
-plt.show()
+# Generate the hopfield energy plot
+# First, generate the hopfield energy list
+for ei in range(len(clamped_energy)):
+    total_energy.append(free_energy[ei] + beta * clamped_energy[ei])
+plt.plot(range(len(total_energy)), total_energy)
+plt.title("Hopfield Energy over time")
+plt.xlabel("Episode #")
+plt.ylabel("Hopfield Energy")
+plt.savefig("hopfield_energy.png")
+plt.close()
+
+# Generate the hopfield energy plot for episode 500 and onward
+plt.plot(range(len(total_energy)-500), total_energy[500:])
+plt.title("Hopfield Energy over time (skipping to episode 500)")
+plt.xlabel("Episode # after 500")
+plt.ylabel("Hopfield Energy")
+plt.savefig("hopfield_energy_skipping.png")
+plt.close()
+
+# Generate the hopfield energy plot for episode 500 and onward, with averaging filter as well
+plt.plot(range(len(total_energy)-500), np.convolve(total_energy[500:], avg_filter, mode='same'))
+plt.title("Hopfield Energy over time (skipping to episode 500)")
+plt.xlabel("Episode # after 500")
+plt.ylabel("Hopfield Energy")
+plt.savefig("hopfield_energy_skipping_smoothed.png")
+plt.close()
+
+# fig, ax = plt.subplots()
+# plt.title("MSE and accuracy of Equalibrium Prop")
+# mse_x=np.arange(len(mse_list))
+
+# MSE = ax.plot(mse_list, color='tab:blue', label='MSE')
+
+# ax.tick_params(axis='y', labelcolor='tab:blue' )
+# #ax.legend()
+# ax2 = ax.twinx()
+# acc=ax2.scatter(test_acc_x, test_acc, color='tab:green', label = 'Test Accuracy')
+# ax2.plot(accuracy_list, color='tab:orange', label = 'Batch Accuracy')
+
+# ax2.set_ylim(0,1)
+# acc.set_zorder(20)
+# ax2.tick_params(axis='y', labelcolor='tab:orange')
+# #fig.legend(loc='upper right')
+# plt.grid()
+# plt.show()
